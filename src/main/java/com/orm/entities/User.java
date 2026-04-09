ebe3d8e0ad57d3cbd2be0ab3ca2c5955b1aa4aec
rahul-kumar25 <rahul.kumar@clivertech.com>
2026-04-09 10:11:04 +0530

    update

--- src/main/java/com/orm/entities/User.java
+++ src/main/java/com/orm/entities/User.java
@@ -1,50 +1,66 @@
 package com.orm.entities;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+//@Table
 public class User {
 
+	@Id
 	private long id;
 	private String name;
 	private String email;
 	private String password;
 	private String gender;
 	private String city;
+
 	public long getId() {
 		return id;
 	}
+
 	public void setId(long id) {
 		this.id = id;
 	}
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	public String getEmail() {
 		return email;
 	}
+
 	public void setEmail(String email) {
 		this.email = email;
 	}
+
 	public String getPassword() {
 		return password;
 	}
+
 	public void setPassword(String password) {
 		this.password = password;
 	}
+
 	public String getGender() {
 		return gender;
 	}
+
 	public void setGender(String gender) {
 		this.gender = gender;
 	}
+
 	public String getCity() {
 		return city;
 	}
+
 	public void setCity(String city) {
 		this.city = city;
 	}
-	
-	
-	
+
 }
