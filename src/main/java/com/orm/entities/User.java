1a5055944f4efb932bce8471b32a184902746476
rahul-kumar25 <rahul.kumar@clivertech.com>
2026-04-09 10:00:16 +0530

    update

--- src/main/java/com/orm/entities/User.java
+++ src/main/java/com/orm/entities/User.java
@@ -2,4 +2,49 @@ package com.orm.entities;
 
 public class User {
 
+	private long id;
+	private String name;
+	private String email;
+	private String password;
+	private String gender;
+	private String city;
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	
+	
+	
 }
